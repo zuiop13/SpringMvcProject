4d9c165ec7a5dd72666c16fa1f0fca37fdd44361
zuiop13 <zuiop13@naver.com>
2021-06-16 13:47:24 +0900

    정성훈 - 2021 06 16 - 동물 자원 반영

--- src/main/java/Controller/system/SystemConstants.java
+++ src/main/java/Controller/system/SystemConstants.java
@@ -9,8 +9,8 @@ public class SystemConstants {
     public static final String PW = "changeme";
 
     //DNF api 정보
-    public static final String DNF_DOMAIN = "https://api.neople.co.kr";
-    public static final String DNF_KEY    = "rBlL4o1MNlRGkZY3p6FeAOSTkMjKQ0r6";
+    public static final String DNF_DOMAIN       = "https://api.neople.co.kr";
+    public static final String DNF_KEY          = "rBlL4o1MNlRGkZY3p6FeAOSTkMjKQ0r6";
     public static final String DNF_SERVERS_PATH = " df/servers";
 
     //퍗샵,병원 api 정보
@@ -21,8 +21,8 @@ public class SystemConstants {
     public static final String KOSIS_DOMAIN = "http://openapi.animal.go.kr/openapi/service/rest/animalShelterSrvc/shelterInfo";
     public static final String KOSIS_KEY    = "8TnPQAwfcuTSzCAnZqIJvW80asnyB7cZtyyYZcgB8z0lTie4aGNd7TTg9JK8OFt6v%2BsyNmEf162SvTGZi3B%2BvA%3D%3D";
 
-    // xml 파일경로
-    public static final String XML_PATH    = "C:/Users/PRO/Desktop/동물보호복지온라인/fulldata_01.xml";
+    public static final String XML_PATH     = "/usr/local/tomcat/webapps/SpringMvcProject/file/fulldata_01.xml";  // 운영 파일경로
+    //public static final String XML_PATH   = "C:/Users/PRO/Desktop/동물보호복지온라인/fulldata_01.xml";            // xml 파일경로
 
     //DB 정보
     public static final String DB_URL    = "jdbc:postgresql://3.36.203.84:5432/springdata"; //운영
